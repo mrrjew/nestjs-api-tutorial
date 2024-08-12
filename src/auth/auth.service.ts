a36ede74c3954d7bc21d320c755690342ba669de
mrr.jew <100996700+mrrjew@users.noreply.github.com>
2024-08-12 06:45:25 +0000

    Pending changes exported from your codespace

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,14 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon2 from "argon2"
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 // import {User,BookMark} from "@prisma/client"
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService){}
+    constructor(private prisma: PrismaService, private jwt: JwtService, private config:ConfigService){}
 
     async signup(dto:AuthDto){
         try{
@@ -55,5 +57,18 @@ export class AuthService {
         delete user.hash
 
         return user
+    }
+
+    async signToken(userId:number,email:string):Promise<string>{
+        const payload = {
+            sub:userId,
+            email
+        }
+
+        const secret = this.config.get('JWT_SECRET')
+        return this.jwt.signAsync(payload,{
+            expiresIn:'15m',
+            secret
+        })
     }
 }
